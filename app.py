ead0b9c8108fa0e32c40ea923d90c7db09e2caf9
parthpadhya157 <padhyaparth@gmail.com>
2025-09-03 00:21:50 +0530

    New Files

---
+++ app.py
@@ -0,0 +1,10 @@
+# Basic Python code with if condition and while loop example
+
+number = 0
+
+while number < 5:
+    if number % 2 == 0:
+        print(f"{number} is even")
+    else:
+        print(f"{number} is odd")
+    number += 1
